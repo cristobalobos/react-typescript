4c3e0318eac380b599b6c6c3f9424bd8e0fdedf0
cristobalobos <cristobal.lobost@gmail.com>
2020-04-17 16:17:16 -0400

    agregar componentes sin estado

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Message from './message'
 
 class App extends Component {
   render() {
-    //let firstValue: string = 'Manny';
-    //let firstValue: number = 34;
-    // let firstValue: boolean = true;
-    // let firstValue: number[] = [2, 3, 56];
-    //let firstValue: Array<string> = ['2', '3', 'Manny'];
-    
-    // tuple
-    let aTuple: [string, number] = ['Manny',34]
-
-    // enum
-    enum Codes {First = 1, Second = 2 };
-
-    // any
-    let firstName: any = 'Manny';
-
-    // void
-    const warning = ():void =>{
-      console.log('warning')
-    }
-
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            El valor {firstName} es de  {typeof aTuple[0]} type!
+            <Message message='this is a simple message'/>
           </p>
         </header>
       </div>
