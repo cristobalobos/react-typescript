91fb366c9903e986f559b227b978b56eb384241f
cristobalobos <cristobal.lobost@gmail.com>
2020-04-17 16:34:41 -0400

    agregar componentes de estado

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 import Message from './message'
 
-class App extends Component {
+class App extends Component<any> {
+  componentWillMount() {
+    console.log("almost there ...");
+  }
+
+  componentDidMount(){
+    console.log("finally hello ...");
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            <Message message='this is a simple message'/>
+            <Message message='this is a simple message' />
           </p>
         </header>
       </div>
